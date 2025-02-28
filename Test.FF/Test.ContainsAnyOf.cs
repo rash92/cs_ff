64a6eafebcd1db44966c054d96a4c0cc1fd05936
srnissen <4851097+SRNissen@users.noreply.github.com>
2025-02-28 13:57:14 +0100

    null robustness in FF.ContainsAnyOf

--- Test.FF/Test.ContainsAnyOf.cs
+++ Test.FF/Test.ContainsAnyOf.cs
@@ -2,37 +2,83 @@ using snns;
 
 namespace Test_ContainsAnyOf;
 
-// public class ReturnsFalseWhen
-// {
+public class ReturnsFalseWhen
+{
+	[Test]
+	public void InputIsTwoNonOverlappingLists()
+	{
+		//Arrange
+		var x = new List<int> { 1, 2, 3 };
+		var y = new List<int> { 4, 5, 6 };
 
-// 	[Test]
-// 	public void DefaultTest()
-// 	{
-// 		Assert.Fail();
-// 	}
+		//Act
+		var result = FF.ContainsAnyOf(x, y);
 
-// 	[Test]
-// 	public void OuterIsNull()
-// 	{
-// 		Assert.Fail();
-// 	}
+		//Assert
+		Assert.That(result, Is.EqualTo(false));
+	}
 
-// 	[Test]
-// 	public void InnerIsNull()
-// 	{
-// 		Assert.Fail();
-// 	}
+	[Test]
+	public void OuterIsNullOrEmpty()
+	{
+		List<int>? outer = null;
+		List<int>? inner = new List<int> { 1, 2, 3 };
 
-// }
+		Assert.That(FF.ContainsAnyOf(outer, inner), Is.EqualTo(false));
+		outer = new();
+		Assert.That(FF.ContainsAnyOf(outer, inner), Is.EqualTo(false));
+	}
 
-// public class ReturnsTrueWhen
-// {
-// 	[Test]
-// 	public void OneSharedElement()
-// 	{
-// 		Assert.Fail();
-// 	}
-// }
+	[Test]
+	public void InnerIsNullOrEmpty()
+	{
+		List<int>? outer = new List<int> { 1, 2, 3 };
+		List<int>? inner = null;
+
+		Assert.That(FF.ContainsAnyOf(outer, inner), Is.EqualTo(false));
+		inner = new();
+		Assert.That(FF.ContainsAnyOf(outer, inner), Is.EqualTo(false));
+	}
+}
+
+public class ReturnsTrueWhen
+{
+	[Test]
+	public void OneSharedElement()
+	{
+		var x = new List<int> { 1, 2, 3 };
+		var y = new List<int> { 1 };
+		Assert.That(FF.ContainsAnyOf(x, y), Is.EqualTo(true));
+		Assert.That(FF.ContainsAnyOf(y, x), Is.EqualTo(true));
+	}
+
+	[Test]
+	public void MultipleSharedElements()
+	{
+		var x = new List<int> { 1, 2, 3 };
+		var y = new List<int> { 1, 2 };
+		Assert.That(FF.ContainsAnyOf(x, y), Is.EqualTo(true));
+		Assert.That(FF.ContainsAnyOf(y, x), Is.EqualTo(true));
+	}
+
+	[Test]
+	public void AllSharedElements()
+	{
+		var x = new List<int> { 1, 2, 3 };
+		var y = new List<int> { 1, 2, 3 };
+		Assert.That(FF.ContainsAnyOf(x, y), Is.EqualTo(true));
+		Assert.That(FF.ContainsAnyOf(y, x), Is.EqualTo(true));
+	}
+
+	[Test]
+	public void SharedElementsOutOfOrder()
+	{
+		var x = new List<int> { 1, 2, 3 };
+		var y = new List<int> { 3, 2, 1 };
+		Assert.That(FF.ContainsAnyOf(x, y), Is.EqualTo(true));
+		Assert.That(FF.ContainsAnyOf(y, x), Is.EqualTo(true));
+	}
+}
 
 public class DoesNotThrowForIntCollectionsWhen
 {
@@ -205,5 +251,18 @@ public class DoesNotThrowWhenForStrings
 		var outer = new List<string> { "a", "b", "c" };
 		var inner = new string?[] { null, "d", "e" };
 		Assert.DoesNotThrow(() => FF.ContainsAnyOf(outer, inner));
+	}
+}
+
+public class IsRobustAgainst
+{
+	[Test]
+	public void NullElements()
+	{
+		var x = new List<string?> { null, "", "abc" };
+		var y = new List<string?> { null, null };
+
+		Assert.That(FF.ContainsAnyOf(x, y), Is.EqualTo(true));
+		Assert.That(FF.ContainsAnyOf(y, x), Is.EqualTo(true));
 	}
 }
